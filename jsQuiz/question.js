16a0d124b8ab6d44ce43d1b421d86008307e635e
Helen Tsui <helentsui2011@gmail.com>
2019-10-02 11:42:18 -0400

    edit jsQuiz

--- jsQuiz/question.js
+++ jsQuiz/question.js
@@ -1,16 +1,14 @@
 class Question {
    constructor (text, choices, answer){
-    this.text = text;
-    this.choices = choices;
-    this.answer = answer;
-}
-}
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+    }
 
-var questions = [
-new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
-new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
-new Question("What has not been covered in PUI lab?", ["HTML", "CSS","Javascript", "Java"], "Java"),
-];
+    isCorrectAnswer(choice){
+        return this.answer === choice;
+    }
+}
 
 
 class Quiz {
@@ -18,26 +16,28 @@ class Quiz {
         this.score = 0;
         this.questions = questions;
         this.questionIndex = 0;
-    }   
-}
+    }  
 
-getQuestion() {
-    return this.questions[this.questionIndex];
-}
+    getQuestion() {
+        return this.questions[this.questionIndex];
+    }
+    
+    isEnded() {
+        return this.questionIndex === this.questions.length;
+    }
 
-
-isCorrectAnswer(choice){
-    return this.answer === choice;
-}
-
-
-guess(answer) {
-    if(this.getQuestion().isCorrectAnswer(answer)) {
-        this.score++;
+    guess(answer) {
+        if(this.getQuestion().isCorrectAnswer(answer)) {
+            this.score++;
+        }
         this.questionIndex++;
+    }
+
 }
 
-function guess(id, guess) {
+
+
+function guessAnswer(id, guess) {
     var button = document.getElementById(id);
     button.onclick = function() {
      quiz.guess(guess);
@@ -46,6 +46,10 @@ function guess(id, guess) {
 };
 
 function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
 
      var element = document.getElementById("question");
      element.innerHTML = quiz.getQuestion().text;
@@ -55,14 +59,28 @@ function populate() {
      for(var i = 0; i < choices.length; i++) {
          var element = document.getElementById("choice" + i);
          element.innerHTML = choices[i];
-         guess("btn" + i, choices[i]);
+         guessAnswer("btn" + i, choices[i]);
      }
- 
-    
+ }
 };
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+var questions = [
+new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
+new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+new Question("What has not been covered in PUI lab?", ["HTML", "CSS","Javascript", "Java"], "Java"),
+];
 
 var quiz = new Quiz(questions);
 populate();
+
+
 
 
 

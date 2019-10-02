845c719aca2838a00aa66fd94d561c3c4c746772
Helen Tsui <helentsui2011@gmail.com>
2019-10-02 11:18:15 -0400

    Update question.js

--- jsQuiz/question.js
+++ jsQuiz/question.js
@@ -21,25 +21,14 @@ class Quiz {
     }   
 }
 
-class Quiz {
-        constructor(questions) {
-            this.score = 0;
-    this.questions = questions;
-            this.questionIndex = 0;
-}   
-
-    
-    getQuestion() {
-
-        return this.questions[this.questionIndex];
-    
+getQuestion() {
+    return this.questions[this.questionIndex];
 }
 
-}
 
 isCorrectAnswer(choice){
-             return this.answer === choice;
-            }
+    return this.answer === choice;
+}
 
 
 guess(answer) {

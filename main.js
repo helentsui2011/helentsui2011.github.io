1b440d7c6673c75c938ec0135b822f691c49d102
Helen Tsui <helentsui2011@gmail.com>
2019-10-25 20:49:33 -0400

    edit

---
+++ main.js
@@ -0,0 +1,13 @@
+ var count = 0;
+      var button = document.getElementById("countButton");
+      var display = document.getElementById("displayCount");
+
+      button.onclick = function(){
+        count++;
+        display.innerHTML = count;
+      }
+
+
+function store() {
+    localStorage.setItem()
+}

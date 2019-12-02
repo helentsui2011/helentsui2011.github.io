ceb84c0b97ff27aff928cd6d5610aa46b1505a4c
Helen Tsui <helentsui2011@gmail.com>
2019-12-01 20:14:49 -0500

    hw8 final project start

---
+++ hw8-final/main.js
@@ -0,0 +1,12 @@
+// $('.home-image').click(function() {
+//     changeColor(this);
+// });
+
+// how to add a lightbox 
+// https://www.w3schools.com/howto/howto_js_lightbox.asp
+
+
+
+// window.onload = function() {
+//     if 
+// }

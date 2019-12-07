dfc4b9aac153db58877302b07d2fac5dc084b9af
Helen Tsui <helentsui2011@gmail.com>
2019-12-06 23:00:40 -0500

    Update main.js

--- hw8-final/js/main.js
+++ hw8-final/js/main.js
@@ -41,76 +41,76 @@ $('.dark').click(function() {
     });
 
 
-// (function () {
-//     'use strict';
+(function () {
+    'use strict';
     
-//     var img = document.querySelector('img'),
-//         list = document.querySelector('ul'),
-//         section = document.querySelector('section'),
-//         paletteReady = false;
+    var img = document.querySelector('img'),
+        list = document.querySelector('ul'),
+        // section = document.querySelector('section'),
+        paletteReady = false;
         
-//     img.addEventListener('load', function() {
-//         if ( !paletteReady )
-//             getPalette();
-//     });
+    img.addEventListener('load', function() {
+        if ( !paletteReady )
+            getPalette();
+    });
     
-//     if (!paletteReady)
-//         getPalette();
+    if (!paletteReady)
+        getPalette();
     
-//     function getPalette() {
-//         paletteReady = true;
+    function getPalette() {
+        paletteReady = true;
         
-//         var vibrant = new Vibrant(img),
-//             swatches = vibrant.swatches(),
-//             listFragment = new DocumentFragment();
+        var vibrant = new Vibrant(img),
+            swatches = vibrant.swatches(),
+            listFragment = new DocumentFragment();
         
-//         for ( var swatch in swatches ) {
-//             if (swatches.hasOwnProperty(swatch) && swatches[swatch]) { 
-//                 console.log(swatch, swatches[swatch].getHex());
-//                 var li = document.createElement('li'),
-//                     p = document.createElement('p'),
-//                     small = document.createElement('small');
+        for ( var swatch in swatches ) {
+            if (swatches.hasOwnProperty(swatch) && swatches[swatch]) { 
+                console.log(swatch, swatches[swatch].getHex());
+                var li = document.createElement('li'),
+                    p = document.createElement('p'),
+                    small = document.createElement('small');
                 
-//                 p.textContent = swatches[swatch].getHex();
-//                 p.style.color = swatches[swatch].getTitleTextColor();
-//                 small.textContent = swatch;
-//                 small.style.color = swatches[swatch].getBodyTextColor();
-//                 li.style.backgroundColor = swatches[swatch].getHex();
-//                 li.appendChild(p);
-//                 li.appendChild(small);
-//                 listFragment.appendChild(li);
-//             }
-//         }
+                p.textContent = swatches[swatch].getHex();
+                p.style.color = swatches[swatch].getTitleTextColor();
+                small.textContent = swatch;
+                small.style.color = swatches[swatch].getBodyTextColor();
+                li.style.backgroundColor = swatches[swatch].getHex();
+                li.appendChild(p);
+                li.appendChild(small);
+                listFragment.appendChild(li);
+            }
+        }
         
-//         list.appendChild(listFragment);
+        list.appendChild(listFragment);
         
-//         if (swatches['DarkVibrant']) {
-//             section.style.backgroundColor = swatches['DarkVibrant'].getHex();
-//         }
-//     }
-// } ());
+        if (swatches['DarkVibrant']) {
+            section.style.backgroundColor = swatches['DarkVibrant'].getHex();
+        }
+    }
+} ());
 
 
  
-var img = document.createElement('img');
-img.setAttribute('src', 'images/me.jpg')
+// var img = document.createElement('img');
+// img.setAttribute('src', 'images/me.jpg')
 
-img.addEventListener('load', function() {
-    var vibrant = new Vibrant(img);
-    var swatches = vibrant.swatches()
-    for (var swatch in swatches)
-        if (swatches.hasOwnProperty(swatch) && swatches[swatch])
-            console.log(swatch, swatches[swatch].getHex())
+// img.addEventListener('load', function() {
+//     var vibrant = new Vibrant(img);
+//     var swatches = vibrant.swatches()
+//     for (var swatch in swatches)
+//         if (swatches.hasOwnProperty(swatch) && swatches[swatch])
+//             console.log(swatch, swatches[swatch].getHex())
 
-    /*
-     * Results into:
-     * Vibrant #7a4426
-     * Muted #7b9eae
-     * DarkVibrant #348945
-     * DarkMuted #141414
-     * LightVibrant #f3ccb4
-     */
-});
+//     /*
+//      * Results into:
+//      * Vibrant #7a4426
+//      * Muted #7b9eae
+//      * DarkVibrant #348945
+//      * DarkMuted #141414
+//      * LightVibrant #f3ccb4
+//      */
+// });
         
 
 

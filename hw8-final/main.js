5624fce5a6026d12dcd1f4e24c3445cee5fddb2a
Helen Tsui <helentsui2011@gmail.com>
2019-12-06 20:52:10 -0500

    vibrant

--- hw8-final/main.js
+++ hw8-final/main.js
@@ -22,6 +22,26 @@ var r = ["Saab", "Volvo", "BMW"];
 
 
  
+var img = document.createElement('img');
+img.setAttribute('src', 'images/me.jpg')
+
+img.addEventListener('load', function() {
+    var vibrant = new Vibrant(img);
+    var swatches = vibrant.swatches()
+    for (var swatch in swatches)
+        if (swatches.hasOwnProperty(swatch) && swatches[swatch])
+            console.log(swatch, swatches[swatch].getHex())
+
+    /*
+     * Results into:
+     * Vibrant #7a4426
+     * Muted #7b9eae
+     * DarkVibrant #348945
+     * DarkMuted #141414
+     * LightVibrant #f3ccb4
+     */
+});
+        
 
 
 // function draw(img) {

2783f7e73bdb41355c113176f482db69ec864fbf
Helen Tsui <helentsui2011@gmail.com>
2019-12-06 21:57:05 -0500

    up

--- hw8-final/main.js
+++ hw8-final/main.js
@@ -20,28 +20,6 @@ span.onclick = function() {
 
 
 
- 
-var img = document.createElement('img');
-img.setAttribute('src', 'images/me.jpg')
-
-img.addEventListener('load', function() {
-    var vibrant = new Vibrant(img);
-    var swatches = vibrant.swatches()
-    for (var swatch in swatches)
-        if (swatches.hasOwnProperty(swatch) && swatches[swatch])
-            console.log(swatch, swatches[swatch].getHex())
-
-    /*
-     * Results into:
-     * Vibrant #7a4426
-     * Muted #7b9eae
-     * DarkVibrant #348945
-     * DarkMuted #141414
-     * LightVibrant #f3ccb4
-     */
-});
-        
-
 
 // function draw(img) {
 //     var canvas = document.createElement("canvas");
@@ -205,5 +183,27 @@ $('.dark').click(function() {
     }
 } ());
 
+
+ 
+var img = document.createElement('img');
+img.setAttribute('src', 'images/me.jpg')
+
+img.addEventListener('load', function() {
+    var vibrant = new Vibrant(img);
+    var swatches = vibrant.swatches()
+    for (var swatch in swatches)
+        if (swatches.hasOwnProperty(swatch) && swatches[swatch])
+            console.log(swatch, swatches[swatch].getHex())
+
+    /*
+     * Results into:
+     * Vibrant #7a4426
+     * Muted #7b9eae
+     * DarkVibrant #348945
+     * DarkMuted #141414
+     * LightVibrant #f3ccb4
+     */
+});
+        
 
 
